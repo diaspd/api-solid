0aaaa2cf49690f75b6e38c6575388dd8fd2528ca
Pedro Dias <pdr050203@gmail.com>
2025-08-21 08:31:26 -0300

    chore: configure environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import { app } from '../src/app'
+import { env } from './env'
 
 app.listen({
   host: '0.0.0.0',
-  port: 3333,
+  port: env.PORT,
 }).then((port) => {
   console.log(`🚀 HTTP Server Running on port ${port}`)
 })
