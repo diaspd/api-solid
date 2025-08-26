0503ae147a116731ee24aff29bf1cdd08780c256
Pedro Dias <pdr050203@gmail.com>
2025-08-26 08:15:43 -0300

    chore: add eslint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import { app } from '../src/app'
-import { env } from './env'
+import { app } from "../src/app";
+import { env } from "./env";
 
-app.listen({
-  host: '0.0.0.0',
-  port: env.PORT,
-}).then((port) => {
-  console.log(`🚀 HTTP Server Running on port ${port}`)
-})
+app
+  .listen({
+    host: "0.0.0.0",
+    port: env.PORT,
+  })
+  .then((port) => {
+    console.log(`🚀 HTTP Server Running on port ${port}`);
+  });
